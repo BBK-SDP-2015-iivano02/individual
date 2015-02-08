4feedb412097ca75e2043069b4fd64b7aae34c55
Ilya Ivanov <contact@ilyaivanov.org>
2015-02-08 21:11:21 +0000

    Removed unused variables

--- src/sml/BnzInstruction.java
+++ src/sml/BnzInstruction.java
@@ -8,9 +8,7 @@ package sml;
 
 public class BnzInstruction extends Instruction {
 
-	private int result;
 	private int op1;
-	private int op2;
 
 	public BnzInstruction(String label, String op) {
 		super(label, op);
@@ -23,9 +21,6 @@ public class BnzInstruction extends Instruction {
 
 	@Override
 	public void execute(Machine m) {
-		int value1 = m.getRegisters().getRegister(op1);
-		int value2 = m.getRegisters().getRegister(op2);
-		m.getRegisters().setRegister(result, value1 / value2);
 	}
 
 	@Override
